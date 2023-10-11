c0a7add00d2577f273ddfded746a84b617af86f8
Markus The <markus@mobzystems.nl>
2023-10-11 23:21:38 +0200

    Safari does not support static class fields!

--- src/BackendService.ts
+++ src/BackendService.ts
@@ -2,38 +2,40 @@ import { Child, Command } from "@tauri-apps/api/shell";
 
 export type BackendServiceState = 'stopped' | 'started' | 'running';
 
-export class BackendService {
-    private static state: BackendServiceState = 'stopped';
-    private static process: Child | undefined = undefined;
+// Safari does not support static class fields!
+// We have to leave these as module variables then
+let _state: BackendServiceState = 'stopped';
+let _process: Child | undefined = undefined;
 
+export class BackendService {
     /***
      * Start the service at the supplied url (passed as --urls argument)
      * Can only be called when state === 'stopped' and process is undefined
      * Returns new state: either 'started' if successful, or 'stopped' if not
      */
     static async start(url: string) {
-        console.log(`Starting service, state is ${this.state}`);
-        if (this.state !== 'stopped' || this.process !== undefined)
-            return this.state;
+        console.log(`Starting service, state is ${_state}`);
+        if (_state !== 'stopped' || _process !== undefined)
+            return _state
 
         // Change state ASAP to prevent others starting another instance with us
-        this.state = 'started';
+        _state = 'started';
 
         try {
             const command = new Command('services', ['--urls', url]);
             const process = await command.spawn();
 
-            this.process = process;
+            _process = process;
 
             console.log(`BackendService: process #${process.pid} started`);
         }
         catch (error: any) {
-            this.state = 'stopped';
+            _state = 'stopped';
             
             console.log(`BackendService: failed to start process: '${error}'`);
         }
 
-        return this.state;
+        return _state;
     }
 
     /***
@@ -42,8 +44,8 @@ export class BackendService {
      * Sets state to 'running' when done, or 'stopped' if failed after retries
     */
     static async waitUntilReady(testUrl: string) {
-        if (this.state !== 'started')
-            return this.state;
+        if (_state !== 'started')
+            return _state;
 
         const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
         let success = false;
@@ -64,12 +66,12 @@ export class BackendService {
         if (success) {
             // await sleep(5000); // For testing
             console.log(`BackendService: service started successfully`);
-            this.state = 'running';
+            _state = 'running';
         } else {
             console.log(`BackendService: service was NOT started successfully`);
             await this.stop();
         }
-        return this.state;
+        return _state;
     }
 
     /***
@@ -77,20 +79,20 @@ export class BackendService {
      * Sets state to 'stopped' and process to undefined, even on error
      */
     static async stop() {
-        if (this.state === 'stopped' || this.process === undefined)
-            return this.state;
+        if (_state === 'stopped' || _process === undefined)
+            return _state;
 
         try {
-            await this.process?.kill();
-            console.log(`BackendService: stopped process #${this.process.pid}`);
+            await _process?.kill();
+            console.log(`BackendService: stopped process #${_process.pid}`);
         }
         catch (error: any) {
-            console.log(`BackendService: error stopping process #${this.process.pid}`);
+            console.log(`BackendService: error stopping process #${_process.pid}`);
         }
 
-        this.state = 'stopped';
-        this.process = undefined;
+        _state = 'stopped';
+        _process = undefined;
 
-        return this.state;
+        return _state;
     }
 }
